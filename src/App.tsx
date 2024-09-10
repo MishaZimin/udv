cc019f39959ee7f8e03a00dd2906f24636922202
ZiminMisha <mikhail.zimin.2004@bk.ru>
2024-09-10 22:00:41 +0500

    create app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="flex h-svh">
+                <p className="mx-auto my-auto font-mono text-2xl">U D V</p>
+            </div>
+        </>
+    );
+}
+
+export default App;
