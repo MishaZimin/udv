fda25dc5718b1e14098345f13361c1c7b7ad4479
ZiminMisha <mikhail.zimin.2004@bk.ru>
2024-10-30 16:44:46 +0500

    fix build

--- src/features/submit-benefit-btn/ui/SubmitBenefitBtn.tsx
+++ src/features/submit-benefit-btn/ui/SubmitBenefitBtn.tsx
@@ -1,14 +1,11 @@
-import { useCardStore, useCheckboxStore } from "src/pages/settings";
 import { validateFields } from "src/pages/settings/helpers/validate";
 import { useCreateBenefitLogic } from "src/features/submit-benefit-btn/model/hooks/create/useCreateBenefitLogic";
-import { useEditBenefit } from "src/features/submit-benefit-btn/model/hooks/edit/useEditBenefit";
 import { Button } from "src/shared/ui";
 import {
   getFromLocalStorage,
   saveToLocalStorage,
 } from "src/shared/lib/local-storage";
 import { useEditBenefitLogic } from "../model/hooks/edit/useEditBenefitLogic";
-import Loader from "src/shared/ui/loader/Loader";
 
 export const SubmitButton = () => {
   const { handleSubmit, errorCreate } = useCreateBenefitLogic();
@@ -19,7 +16,11 @@ export const SubmitButton = () => {
   }
 
   const handleClick = () => {
-    getFromLocalStorage("edit-benefit-id") ? handleEdit() : handleSubmit();
+    if (getFromLocalStorage("edit-benefit-id")) {
+      handleEdit();
+    } else {
+      handleSubmit();
+    }
     saveToLocalStorage("edit-benefit-id", null);
   };
 
