20eff97e5d7f1763edb048ba83afbda857c740d7
ZiminMisha <mikhail.zimin.2004@bk.ru>
2024-10-31 14:20:53 +0500

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "prompt",
-      workbox: {
-        disableDevLogs: true,
-        runtimeCaching: [],
-      },
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: "udv",
-        short_name: "udv",
-        description: "udv",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "src/shared/assets/svgs/Logo2.svg",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "src/shared/assets/svgs/Logo2.svg",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       src: "/src",
